b0211b41f613a05d524906f4e9fab3fea164826c
olegnikitindev <olegnikitindev>
2017-09-19 20:37:10 +0300

    Added more info

--- app/routes/node_routes.js
+++ app/routes/node_routes.js
@@ -1,9 +1,12 @@
 module.exports = function (app, db) {
-
-};
-
-module.exports = function (app, db) {
     app.post('/notes', (req, res) => {
-        res.send('Hello')
+        const note = {text: req.body.body, title: req.body.title};
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(result.ops[0]);
+            }
+        })
     });
 };
